4b63afd5f34f02ed4ff851367091acff5570d5c3
Eduardo Bento <80079390+duzin12@users.noreply.github.com>
2022-02-21 12:30:34 -0300

    Add files via upload

---
+++ PythonExerciciosmundo1/DESAFIO02.py
@@ -0,0 +1,8 @@
+dia = input('Qual é o seu dia de Nascimento?')
+mês = input('Em que mês você Nasceu?')
+ano = input('Em que Ano você Nasceu?')
+
+print('Você Nasceu no dia',dia,'de',mês,'de',ano,' . Correto?')
+
+#correto
+#colher os dados da data de nascimento como dia ,mes e ano , e depois mostrar na tela
---
+++ PythonExerciciosmundo1/DESAFIO03.py
@@ -0,0 +1,7 @@
+n1 = float(input('Digite um número:'))
+n2 = float(input('Digite outro número:'))
+s = n1 + n2
+print('A soma entre {} e {} é {}'.format(n1,n2,s))
+
+#errei e depois corrigi
+#colher 2 numeros e depois obter a soma deles e mostrar na tela
